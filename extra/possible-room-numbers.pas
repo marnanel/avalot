22766df9eb8647af6b110811af89d0496da95127
Thomas Thurman <thomas@thurman.org.uk>
2013-06-18 19:08:01 +0100

    Possible room numbers

---
+++ extra/possible-room-numbers.pas
@@ -0,0 +1,52 @@
+
+
+const r__Nowhere = 0;
+const r__Yours = 1;
+const r__OutsideYours = 2;
+{ A corridor in your castle. = 3 }
+{ Outside your castle = 5; }
+const r__MusicRoom = 7;
+const r__OutsideArgentPub = 9;
+const r__ArgentRoad = 10;
+const r__WiseWomans = 11;
+const r__Spludwicks = 12;
+const r__InsideAbbey = 13;
+const r__OutsideAbbey = 14; { assumed }
+const r__AvvysGarden = 15;
+const r__AylesOffice = 16
+const r__OutsideArgentPub = 19
+const r__BrummieRoad = 20;
+const r__Lusties = 21; { ? not sure }
+const r__EntranceHall = 22
+const r__LustiesRoom = 23;
+const r__WestHall = 25;
+const r__EastHall = 26;
+const r__Oubliette = 27;
+const r__Geidas = 28;
+const r__Catacombs = 29;
+
+{ -------------- }
+
+const r__Bridge = 40;
+const r__Robins = 42;
+const r__OutsideNottsPub = 46;
+const r__NottsPub = 47;
+
+{ -------------- }
+
+const r__OutsideDucks = 50;
+const r__Ducks = 51;
+
+{ -------------- }
+
+const r__OutsideCardiffCastle = 70;
+const r__InsideCardiffCastle = 71;
+
+{ -------------- }
+
+{ place80 appears to be bogus }
+
+{ -------------- }
+
+const r__BossKey = 98; { assumed }
+const r__Map = 99;
